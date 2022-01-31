f985e98b3f1a26f2b5c449b656d9226e68dafb20
Antoine Saunier <antoine.saunier.pro@gmail.com>
2022-01-31 17:19:02 +0100

    refactoring code and basic game without front

--- src/library/games/blackJack.js
+++ src/library/games/blackJack.js
@@ -5,13 +5,13 @@ let deckID = "";
 let remainingCards = 0;
 let dealerCards = [];
 let playerCards = [];
-let roundLost = false;
-let roundWon = false;
-let roundDraw = false;
+let roundLost = 0;
+let roundWon = 0;
+let roundDraw = 0;
 let playerScore = 0;
 let dealerScore = 0;
 
-let roundBet = "";
+let roundBet = 0;
 
 // display of the score of the game
 let dealerScoreDisplay = document.getElementById("dealer-score");
@@ -29,15 +29,21 @@ let doubleButtonDisplay = document.getElementById("double-button");
 let splitButtonDisplay = document.getElementById("split-button");
 let insuranceButtonDisplay = document.getElementById("insurance-button");
 
+let restartButtonDisplay = document.getElementById("restart-button");
+let endButtonDisplay = document.getElementById("end-button");
+
 // On click events
 surrenderButtonDisplay.onclick = surrender;
 startButtonDisplay.onclick = newDeck;
 startButtonDisplay.onclick = newDeck;
 hitButtonDisplay.onclick = () => hitMe('player');
-standButtonDisplay.onclick = () => stay();
+standButtonDisplay.onclick= ()=>setTimeout(()=>dealerPlays(), 600);
+restartButtonDisplay.onclick = replay;
+endButtonDisplay.onclick = end;
 
 export function initBlackJackBet(statusMessage) {
-    document.getElementsByClassName("games-selection")[0].classList.add("hidden");
+    restartButtonDisplay.classList.add("hidden");
+    endButtonDisplay.classList.add("hidden");
     document.getElementById("description-img").src = "dealer.png";
     announcementMessage.textContent = `${statusMessage}`;
     document.getElementsByClassName("player-bet-form")[0].classList.remove("hidden");
@@ -68,6 +74,16 @@ function surrender() {
     }
 }
 
+function replay(){
+    resetPlayingArea();
+    document.getElementsByClassName("blackjack-table")[0].classList.add("hidden");
+    initBlackJackBet("Début de la partie, veuillez donner la valeur de votre mise :")
+}
+
+function end(){
+    resetPlayingArea();
+}
+
 async function newDeck() {
     resetPlayingArea();
     const abortController = new AbortController();
@@ -76,7 +92,7 @@ async function newDeck() {
             if (response.ok) {
                 return response.json();
             }
-            return Promise.reject(new Error("Something went wrong"));
+            return Promise.reject(new Error("Il y a eu une erreur."));
         }).then(function (data) {
             deckID = data.deck_id;
             remainingCards = data.remaining;
@@ -94,74 +110,50 @@ async function newDeck() {
     newHand();
 }
 
-function test() {
-    console.log(deckID);
-}
-
-function hitMe() {
+function hitMe(target) {
     fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
         .then(res => res.json())
         .then(res => {
-            hitButtonDisplay.style.display = "block";
-            standButtonDisplay.style.display = "block";
 
-            playerCards.push(res.cards[0])
-            let cardDomElement = document.createElement("img");
-            cardDomElement.src = res.cards[0].image;
-            playerCardsDisplay.appendChild(cardDomElement);
-            playerScore = computeScore(playerCards);
+            if (target === 'player') {
+                hitButtonDisplay.style.display = "block";
+                standButtonDisplay.style.display = "block";
 
-            if (playerScore > 21) {
+                playerCards.push(res.cards[0])
+                let cardDomElement = document.createElement("img");
+                cardDomElement.src = res.cards[0].image;
+                playerCardsDisplay.appendChild(cardDomElement)
+
+                playerScore = computeScore(playerCards);
+
                 playerScoreDisplay.textContent = `Votre main : ${playerScore}`;
-                roundLost = true;
-                alert("Vous avez perdu, cheh!");
-                // resetPlayingArea();
-            } else if (playerScore === 21) {
-                playerScoreDisplay.textContent = `Votre main : ${playerScore}`;
-                roundWon = true;
-                alert("Vous avez gagné bg !");
-                // resetPlayingArea();
-            } else {
-                playerScoreDisplay.textContent = `Votre main : ${playerScore}`;
+                if (playerScore > 21) {
+                    roundLost = true;
+                    roundLost += 1;
+                    announcementMessage.textContent = "Vous avez perdu ! Voulez vous rejouer ?"
+                    restartButtonDisplay.classList.remove("hidden");
+                    endButtonDisplay.classList.remove("hidden");
+                }
+                else if (playerScore === 21) {
+                    playerScoreDisplay.textContent = `Votre main : ${playerScore}`;
+                    roundWon += 1;
+                    actualiseWallet('+', roundBet * 2);
+                    announcementMessage.textContent = "Vous avez gagné ! Voulez vous rejouer ?"
+                    restartButtonDisplay.classList.remove("hidden");
+                    endButtonDisplay.classList.remove("hidden");
+                }
+            }
+
+            if (target === 'dealer') {
+                let cardDomElement = document.createElement("img");
+                dealerCards.push(res.cards[0])
+                cardDomElement.src = res.cards[0].image;
+                dealerCardsDisplay.appendChild(cardDomElement)
+                dealerPlays();
             }
 
         })
         .catch(console.error)
-}
-
-function stay() {
-    console.log('va baiser ta mère et marche gros fdp', dealerScore)
-    dealerScore = computeScore(dealerCards);
-    while (dealerScore <= 17) {
-        fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
-            .then(res => res.json())
-            .then(res => {
-                hitButtonDisplay.style.display = "disabled";
-                standButtonDisplay.style.display = "disabled";
-
-                dealerCards.push(res.cards[0])
-                let cardDomElement = document.createElement("img");
-                cardDomElement.src = res.cards[0].image;
-                dealerCardsDisplay.appendChild(cardDomElement);
-
-                // if (dealerScore > 21) {
-                //     dealerScoreDisplay.textContent = `Votre main : ${dealerScore}`;
-                //     roundLost = true;
-                //     resetPlayingArea();
-                //     alert("Vous avez perdu, cheh!");
-                // }else if (dealerScore === 21){
-                //     dealerScoreDisplay.textContent = `Votre main : ${dealerScore}`;
-                //     roundWon = true;
-                //     resetPlayingArea();
-                //     alert("Vous avez perdu, cheh!");
-                // }else{
-                dealerScoreDisplay.textContent = `Votre main : ${dealerScore}`;
-                // }
-
-            })
-            .catch(console.error)
-    }
-
 }
 
 function newHand() {
@@ -172,15 +164,16 @@ function newHand() {
             hitButtonDisplay.style.display = "block";
             standButtonDisplay.style.display = "block";
 
-            dealerCards.push(res.cards[0])
-            playerCards.push(res.cards[1], res.cards[2])
-
-            dealerScore = "Main du dealer : ?";
-            dealerScoreDisplay.textContent = dealerScore;
+            dealerCards.push(res.cards[0], res.cards[1])
+            playerCards.push(res.cards[2], res.cards[3])
 
             dealerCards.forEach((card, i) => {
                 let cardDomElement = document.createElement("img");
-                cardDomElement.src = card.image;
+                if(i===0) {
+                    cardDomElement.src = './sbackcard.png';
+                } else {
+                    cardDomElement.src = card.image;
+                }
                 dealerCardsDisplay.appendChild(cardDomElement)
             })
 
@@ -192,13 +185,46 @@ function newHand() {
 
             playerScore = computeScore(playerCards);
             if (playerScore === 21) {
-                roundWon = true;
-                announcementMessage.textContent = "BlackJack! You Win!";
+                roundWon += 1;
+                announcementMessage.textContent = "BlackJack! Vous avez gagné !";
             }
             playerScoreDisplay.textContent = `Votre main : ${playerScore}`;
 
         })
         .catch(console.error)
+}
+
+function dealerPlays() {
+    dealerScore = computeScore(dealerCards);
+    dealerScoreDisplay.textContent = `Main du dealer : ${dealerScore}`;
+    dealerCardsDisplay.firstChild.src = dealerCards[0].image;
+    if (dealerScore < 17) {
+        setTimeout(()=>hitMe('dealer'), 900)
+    }
+    else if (dealerScore > 21) {
+        roundWon += 1;
+        announcementMessage.textContent = "Vous avez gagné ! Voulez vous rejouer ?"
+        restartButtonDisplay.classList.remove("hidden");
+        endButtonDisplay.classList.remove("hidden");
+    }
+    else if (dealerScore > playerScore) {
+        roundLost += 1;
+        announcementMessage.textContent = "Vous avez perdu ! Voulez vous rejouer ?"
+        restartButtonDisplay.classList.remove("hidden");
+        endButtonDisplay.classList.remove("hidden");
+    }
+    else if (dealerScore === playerScore) {
+        roundDraw += 1;
+        announcementMessage.textContent = "Egalité ! Voulez vous rejouer ?"
+        restartButtonDisplay.classList.remove("hidden");
+        endButtonDisplay.classList.remove("hidden");
+    }
+    else {
+        roundWon += 1;
+        announcementMessage.textContent = "Vous avez gagné ! Voulez vous rejouer ?"
+        restartButtonDisplay.classList.remove("hidden");
+        endButtonDisplay.classList.remove("hidden");
+    }
 }
 
 function computeScore(cards) {
@@ -222,14 +248,10 @@ function computeScore(cards) {
 function resetPlayingArea() {
     dealerCards = [];
     playerCards = [];
-    roundLost = false;
-    roundWon = false;
-    roundDraw = false;
-    dealerScore = 0;
+    dealerScore = "?";
     playerScore = 0;
     dealerScoreDisplay.textContent = `Main du dealer : ${dealerScore}`;
     playerScoreDisplay.textContent = `Votre main : ${playerScore}`;
-    announcementMessage.textContent = "";
     while (dealerCardsDisplay.firstChild) {
         dealerCardsDisplay.removeChild(dealerCardsDisplay.firstChild);
     }
